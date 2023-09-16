4d2866a583a26278ffeacf6ad4f7ae9a3fe83ef4
twuebker <33319983+twuebker@users.noreply.github.com>
2023-09-16 21:00:09 +0000

    fix main again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
 import discord
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -7,23 +7,26 @@ client = discord.Client(intents=intents)
 
 
 def run():
-    print("running client...")
-    client.run(os.getenv('TOKEN'))
+  print("running client...")
+  token = os.getenv('TOKEN')
+  if token is None:
+    return
+  client.run(token=token)
 
 
 @client.event
 async def on_ready():
-    print("We have logged in as user {0.user}".format(client))
+  print("We have logged in as user {0.user}".format(client))
 
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+  if message.author == client.user:
+    return
 
-    if message.content.startswith("Apple"):
-        await message.channel.send("Apple God!")
+  if message.content.startswith("Apple"):
+    await message.channel.send("Apple God!")
 
 
 if '__name__' == '__main__':
-    run()
+  run()
